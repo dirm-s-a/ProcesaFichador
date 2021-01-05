8699e57b8bce3758f2b5e82939eb0264de1a116d
“diegodelafuenteDim” <diegodelafuente@dim.com.ar>
2021-01-05 18:42:26 -0300

    Agrego actualizador

--- ProcesaFichador/Formularios/frmLogin.vb
+++ ProcesaFichador/Formularios/frmLogin.vb
@@ -1,13 +1,5 @@
-﻿Imports System.IO
-Imports Microsoft.Office.Interop.Excel
-Imports Employees.Business
-Imports Employees.Entities
-Imports Employees.Data
-Imports Turnos.GlobalFunctions
-Imports DevExpress.XtraGrid.Views.Base
-Imports DevExpress.XtraGrid.Views.Grid
+﻿Imports Employees.Business
 Imports DevExpress.XtraEditors
-Imports DevExpress.Utils.Menu
 Public Class frmLogin
     Inherits XtraForm
 
@@ -72,6 +64,55 @@ Public Class frmLogin
             Return False
         End Try
     End Function
+
+    Private Sub CheckUpdates()
+        Try
+            'Esto es para que corrar el Update, solo si lo corremos desde un exe
+            If Not Debugger.IsAttached Then
+
+                Dim returnValue As String() = Environment.GetCommandLineArgs()
+                If returnValue.Length = 1 Then
+                    If IO.File.Exists(My.Application.Info.DirectoryPath & "\CheckUpdates.exe") Then IO.File.Delete(My.Application.Info.DirectoryPath & "\CheckUpdates.exe")
+                    IO.File.Copy("\\192.168.6.55\Publico\vnNetpAppsNew\FichadorDistrib\CheckUpdates452.exe", My.Application.Info.DirectoryPath & "\CheckUpdates.exe", True)
+                    IO.File.Copy("\\192.168.6.55\Publico\vnNetpAppsNew\FichadorDistrib\Newtonsoft.Json.Net20.dll", My.Application.Info.DirectoryPath & "\Newtonsoft.Json.Net20.dll", True)
+
+                    Const DblQts As String = """"
+
+                    Dim strParam As String = DblQts & Application.ProductVersion & DblQts & " " &
+                                              DblQts & My.Application.Info.DirectoryPath & DblQts & " " &
+                                              DblQts & "\\192.168.6.55\Publico\vnNetpAppsNew\FichadorDistrib\Version.json" & DblQts & " " &
+                                              DblQts & IO.Path.GetFileName(Application.ExecutablePath) & DblQts
+
+                    Process.Start(My.Application.Info.DirectoryPath & "\CheckUpdates.exe ", strParam)
+                    Application.Exit()
+                End If
+                If Not IO.File.Exists(My.Application.Info.DirectoryPath & "\System.Data.dll") AndAlso IO.File.Exists("\\192.168.6.55\Publico\vnNetpAppsNew\FichadorDistrib\System.Data.dll") Then
+                    IO.File.Copy("\\192.168.6.55\Publico\vnNetpAppsNew\FichadorDistrib\System.Data.dll", My.Application.Info.DirectoryPath & "\System.Data.dll", True)
+                End If
+                If IO.File.Exists(My.Application.Info.DirectoryPath & "\pryPbxCallQueue.exe") Then IO.File.Delete(My.Application.Info.DirectoryPath & "\pryPbxCallQueue.exe")
+                If IO.File.Exists(My.Application.Info.DirectoryPath & "\pryPbxCallQueue.exe.config") Then IO.File.Delete(My.Application.Info.DirectoryPath & "\pryPbxCallQueue.exe.config")
+                Call ParcheNewTonSoft()
+            End If
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub ParcheNewTonSoft()
+        Try
+            IO.File.Copy("\\192.168.6.55\Publico\vnNetpAppsNew\FichadorDistrib\Newtonsoft.Json.dll", My.Application.Info.DirectoryPath & "\Newtonsoft.Json.dll", True)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub frmLogin_Load(sender As Object, e As EventArgs) Handles Me.Load
+        'Siempre al principio
+        CheckUpdates()
+    End Sub
+
+
 
 #End Region
 
